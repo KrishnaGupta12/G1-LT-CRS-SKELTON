aba40af1d608382f548e8634fed583e540b8503f
SnehaM06 <snehamulchandani06.sm@gmail.com>
2021-09-15 19:56:44 +0530

    SqlConstants.java with comments

--- src/com/lt/constants/SqlConstants.java
+++ src/com/lt/constants/SqlConstants.java
@@ -2,8 +2,24 @@ package com.lt.constants;
 
 public interface SqlConstants {
 
+    //User table queries
     public final static String USER_TABLE_DATA = "select role from User where username =? && password=?";
     public final static String INSERT_TO_USER = "insert into user values (?,?,?)";
+
+    //Student Table queries
     public final static String INSERT_TO_STUDENT = "insert into Student values (?,?,?,?,?,?,?)";
+
+    //Semester Table queries
+
+    //Course Table queries
+
+    //RegisterCourse Table queries
+
+    //Admin Table queries
+
+    //CourseCatalog Table queries
+
+    //GradeCard Table queries
+
 
 }
